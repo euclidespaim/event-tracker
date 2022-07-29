1eeab89a8390bece36abfd063c9e6e8bba676c93
euclidespaim <euclidespaim@gmail.com>
2022-07-29 15:57:57 -0300

    Refatoranda estados com o recoil

--- src/components/ListaDeEventos/index.tsx
+++ src/components/ListaDeEventos/index.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import { IEvento } from '../../interfaces/IEvento';
+import { useRecoilValue } from 'recoil';
+import { listaDeEventosState } from '../../state/atom';
 import Evento from '../Evento';
 import Filtro from '../Filtro';
 import style from './ListaDeEventos.module.scss';
 
 const ListaDeEventos: React.FC<{ 
-  eventos: IEvento[], 
   aoAlterarStatus: (id: number) => void, 
   aoDeletarEvento: (id: number) => void, 
-  aoFiltroAplicado: (data: Date | null) => void }> = ({ eventos, aoDeletarEvento, aoAlterarStatus, aoFiltroAplicado }) => {
+  aoFiltroAplicado: (data: Date | null) => void }> = ({ aoDeletarEvento, aoAlterarStatus, aoFiltroAplicado }) => {
+
+  const eventos = useRecoilValue(listaDeEventosState)
 
   return (<section>
     <Filtro aoFiltroAplicado={aoFiltroAplicado} />
     <div className={style.Scroll}>
       {eventos.map(evento => (
-        <Evento aoAlterarStatus={aoAlterarStatus} aoDeletarEvento={aoDeletarEvento} evento={evento} key={evento.id} />
+        <Evento 
+          aoAlterarStatus={aoAlterarStatus} 
+          aoDeletarEvento={aoDeletarEvento} 
+          evento={evento} key={evento.id} 
+        />
       ))}
     </div>
   </section>)
---
+++ src/state/atom.ts
@@ -0,0 +1,22 @@
+import { IEvento } from './../interfaces/IEvento';
+import { atom } from "recoil";
+
+export const listaDeEventosState = atom<IEvento[]>({
+  key: "listaDeEventosState",
+  default: [
+    {
+      descricao: "Estudar React",
+      inicio: new Date("2022-01-15T09:00"),
+      fim: new Date("2022-01-15T13:00"),
+      completo: false,
+      id: 1642342747,
+    },
+    {
+      descricao: "Estudar Recoil",
+      inicio: new Date("2022-01-16T09:00"),
+      fim: new Date("2022-01-16T11:00"),
+      completo: false,
+      id: 1642342959,
+    },
+  ],
+});
