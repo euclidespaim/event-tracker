5a19765b4f67e871aaead0465ff3fedf69197b10
euclidespaim <euclidespaim@gmail.com>
2022-08-02 14:48:49 -0300

    Configura json-server

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -1,10 +1,11 @@
+import { eventosAsync } from './seletores/index';
 import { IEvento } from './../interfaces/IEvento';
 import { atom } from "recoil";
 import { IFiltroDeEventos } from '../interfaces/IFiltroDeEventos';
 
 export const listaDeEventosState = atom<IEvento[]>({
   key: "listaDeEventosState",
-  default: [ ],
+  default: eventosAsync,
 });
 
 export const filtroDeEventos = atom<IFiltroDeEventos>({
--- src/state/seletores/index.ts
+++ src/state/seletores/index.ts
@@ -1,3 +1,4 @@
+import { IEvento } from './../../interfaces/IEvento';
 import { filtroDeEventos, listaDeEventosState } from './../atom';
 import { selector } from "recoil";
 
@@ -15,5 +16,18 @@ export const eventosFiltradoState = selector({
             return eHoMesmoDia
         })
         return eventos
+    }
+})
+
+export const eventosAsync = selector({
+    key: "eventosAsync",
+    get: async () => {
+        const respostaHttp = await fetch('http://localhost:8080/eventos')
+        const eventosJson: IEvento[] = await respostaHttp.json()
+        return eventosJson.map(evento => ({
+            ...evento,
+            inicio: new Date(evento.inicio),
+            fim: new Date(evento.fim),
+        }))
     }
 })
